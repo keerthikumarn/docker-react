0f7af5cdd5751fc21353bf3f82c24e16a569964b
Keerthi <n.keerthikumar85@gmail.com>
2021-09-29 11:59:36 +0530

    testing the deployment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hi Raghav....</h1>
+        <h1>Hi Keerthi, Raghav & Vineeth !!!</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
